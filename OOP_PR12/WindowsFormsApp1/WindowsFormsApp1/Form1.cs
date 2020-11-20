bc41b03be8b19f4861cad87999d148ca140b016f
Pavel-Lomov <lpn1972@yandex.ru>
2020-11-20 11:45:11 +1000

    PR12

---
+++ OOP_PR12/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                Kvadrat m = listBox1.Items[listBox1.SelectedIndex] as Kvadrat;
+                m.Stir();
+                toolStripStatusLabel1.Text = "";
+                toolStripStatusLabel2.Text = "";
+                toolStripStatusLabel3.Text = "";
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                if (listBox1.Items.Count == 0)
+                {
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                    редактироватьToolStripMenuItem.Enabled = false;
+                    удалитьToolStripMenuItem.Enabled = false;
+                    toolStripButton2.Enabled = false;
+                    toolStripButton3.Enabled = false;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.BackColor = Color.White;
+            timer1.Start();
+            button2.Enabled = false;
+            button3.Enabled = false;
+            редактироватьToolStripMenuItem.Enabled = false;
+            удалитьToolStripMenuItem.Enabled = false;
+            toolStripButton2.Enabled = false;
+            toolStripButton3.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            f.Text = "Добавление";
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                timer1.Stop();
+                int x = Convert.ToInt32(f.numericUpDown1.Value);
+                int y = Convert.ToInt32(f.numericUpDown2.Value);
+                int l = Convert.ToInt32(f.numericUpDown3.Value);
+                int s = Convert.ToInt32(f.numericUpDown4.Value);
+                Color cl = f.pictureBox1.BackColor;
+                Kvadrat kv = new Kvadrat(x, y, l, s, cl, pictureBox1);
+                listBox1.Items.Add(kv);
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                Kvadrat m = listBox1.Items[i] as Kvadrat;
+                m.Dviz();
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                Form2 f = new Form2();
+                f.Text = "Редактировать";
+                Kvadrat m = listBox1.Items[listBox1.SelectedIndex] as Kvadrat;
+                f.numericUpDown1.Value = m.x;
+                f.numericUpDown2.Value = m.y;
+                f.numericUpDown3.Value = m.l;
+                f.numericUpDown4.Value = m.s;
+                f.pictureBox1.BackColor = m.myColor;
+                if (f.ShowDialog() == DialogResult.OK)
+                {
+                    timer1.Stop();
+                    m.Stir();
+                    int x = Convert.ToInt32(f.numericUpDown1.Value);
+                    int y = Convert.ToInt32(f.numericUpDown2.Value);
+                    int l = Convert.ToInt32(f.numericUpDown3.Value);
+                    int s = Convert.ToInt32(f.numericUpDown4.Value);
+                    Color cl = f.pictureBox1.BackColor;
+                    m.x = x; m.y = y; m.l = l; m.s = s; m.myColor = cl;
+                    listBox1.Items[listBox1.SelectedIndex] = m;
+                    timer1.Start();
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                button2.Enabled = true;
+                button3.Enabled = true;
+                редактироватьToolStripMenuItem.Enabled = true;
+                удалитьToolStripMenuItem.Enabled = true;
+                toolStripButton2.Enabled = true;
+                toolStripButton3.Enabled = true;
+                Kvadrat m = listBox1.Items[listBox1.SelectedIndex] as Kvadrat;
+                toolStripStatusLabel1.Text = "Масштаб: " + m.l;
+                toolStripStatusLabel2.Text = "Скорость: " + m.s;
+                toolStripStatusLabel3.Text = "Цвет: " + m.myColor;
+            }
+        }
+    }
+}
---
+++ OOP_PR12/WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.DialogResult == DialogResult.OK)
+            {
+                int k = 0;
+                try
+                {
+
+                    if (Convert.ToInt32(numericUpDown1.Value) == 0)
+                    {
+                        throw new Exception("Введите X отличный от 0");
+                    }
+                    k++;
+                    if (Convert.ToInt32(numericUpDown2.Value) == 0)
+                    {
+                        throw new Exception("Введите Y отличный от 0");
+                    }
+                    k++;
+                    if (Convert.ToInt32(numericUpDown3.Value) == 0)
+                    {
+                        throw new Exception("Введите масштаб отличный от 0");
+                    }
+                    k++;
+                    if (Convert.ToInt32(numericUpDown4.Value) == 0)
+                    {
+                        throw new Exception("Введите скорость отличную от 0");
+                    }
+                    k++;
+
+                }
+                catch (Exception E)
+                {
+                    if (k == 0) numericUpDown1.Focus();
+                    if (k == 1) numericUpDown2.Focus();
+                    if (k == 2) numericUpDown3.Focus();
+                    if (k == 3) numericUpDown4.Focus();
+                    MessageBox.Show(E.Message);
+                    e.Cancel = true;
+                }
+            }
+        }
+    }
+}
